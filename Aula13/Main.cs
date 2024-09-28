fca5757987973d14c49dfd6ef6a0e4620ac4f0ad
LuucasFeelix <77838383+LuucasFeelix@users.noreply.github.com>
2024-09-28 18:27:47 -0300

    Adicionando Aula13 e o que foi passado na aula

---
+++ Aula13/Main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program{
+    static void Main(){
+        //Tratamento de String Part2
+        string text = "Ola Mundo";
+        // Console.WriteLine(text.Substring(3,4));
+        Console.WriteLine(text.Replace("Ola", "Eae"));
+    }
+}
