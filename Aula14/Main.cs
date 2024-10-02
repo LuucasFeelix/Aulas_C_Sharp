3de4e71ffcb4f318e84c2139fe2178b7a053580f
LuucasFeelix <77838383+LuucasFeelix@users.noreply.github.com>
2024-10-01 22:59:51 -0300

    Aula 14 sobre Class math

---
+++ Aula14/Main.cs
@@ -0,0 +1,13 @@
+using System;
+class Program{
+    static void Main(){
+        //class math
+        Console.WriteLine("A pontecia é: " + Math.Pow(2,3)); // Operador pontencia: ^
+        Console.WriteLine("A Raiz Quadra e: " + Math.Sqrt(9)); // Operador de raiz quadrada
+        Console.WriteLine("Arredondando para o numero mais proximo: " + Math.Round(3.4)); // Operador de arredondar mais proximo
+        Console.WriteLine("Arredonda a cima: " + Math.Ceiling(3.4)); // Arredonda a cima
+        Console.WriteLine("Arredonda a baixo: " + Math.Floor(3.4)); // Arredonda a Baixo
+        Console.WriteLine("O maior numero é: " + Math.Max(3,7)); // Mostra o maior
+        Console.WriteLine("O menor numero é: " + Math.Min(3,7)); // Mostra o menor
+    }
+}
