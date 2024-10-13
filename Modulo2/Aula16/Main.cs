4dd834308a1b3dcd91982c52b74a5e917cc1b58a
LuucasFeelix <77838383+LuucasFeelix@users.noreply.github.com>
2024-10-13 12:43:32 -0300

    Realizado aula 16 Operadores Logicos

--- Modulo2/Aula16/Main.cs
+++ Modulo2/Aula16/Main.cs
@@ -2,6 +2,20 @@ using System;
 
 class Aula16{
     static void Main(){
-    Console.WriteLine("Bora para a aula 16");
+        // Operadores Logicos
+        bool b1 = true;
+        bool b2 = false;
+        int n1 = 20;
+        int n2 = 30;
+
+
+        bool exp = b1 && b2; // AND (e)
+        bool exp2 = b1 || b2; //OR (ou)
+        bool exp3 = b1 == b2 && n1 == n2;
+        
+        Console.WriteLine(exp);
+        Console.WriteLine(exp2);
+        Console.WriteLine(exp3);
+
     }
 }
