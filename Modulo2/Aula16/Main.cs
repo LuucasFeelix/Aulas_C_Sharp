8223a178845cdf126ca61f6fb6a3320e5d4c2ce9
LuucasFeelix <77838383+LuucasFeelix@users.noreply.github.com>
2024-10-16 21:38:44 -0300

    Aula 17 realizando Estrutura Condicional IF

--- Modulo2/Aula16/Main.cs
+++ Modulo2/Aula16/Main.cs
@@ -5,17 +5,12 @@ class Aula16{
         // Operadores Logicos
         bool b1 = true;
         bool b2 = false;
-        int n1 = 20;
-        int n2 = 30;
-
 
         bool exp = b1 && b2; // AND (e)
         bool exp2 = b1 || b2; //OR (ou)
-        bool exp3 = b1 == b2 && n1 == n2;
+        bool exp3 = b1 ! b2; // NOT (não)
         
         Console.WriteLine(exp);
         Console.WriteLine(exp2);
-        Console.WriteLine(exp3);
-
     }
 }
---
+++ Modulo2/Aula17/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class program{
+    static void Main(){
+        //if -> se
+        int n1 = 5;
+
+
+        if(n1 > 10){
+            Console.WriteLine("Olá, Mundo!!");
+        }
+    }
+}
