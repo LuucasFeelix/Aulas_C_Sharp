1ec0ce7b2cef48ecc60fa23824c0e5ee1ba68da0
Lucas Felix Da Silva <luucasfeelix1925@hotmail.com>
2024-12-02 21:56:26 -0300

    Realizado aula 22

--- Modulo2/Aula22/Main.cs
+++ Modulo2/Aula22/Main.cs
@@ -2,6 +2,26 @@ using System;
 
 class program{
     static void Main(){
+        //Estrutura Switch - case
+        Console.WriteLine("Escolha um Opção");
+        Console.WriteLine("[1] - Opção 1");
+        Console.WriteLine("[2] - Opção 2");
+        Console.WriteLine("[3] - Opção 3");
+        int res = Convert.ToInt32(Console.ReadLine());
 
+        switch(res){
+            case 1: 
+                Console.WriteLine("Voce Escolheu a Opção 1");
+                break;
+            case 2:
+                Console.WriteLine("Voce Escolheu a Opção 2");
+                break;
+            case 3:
+                Console.WriteLine("Voce Escolheu a Opção 3");
+                break;
+            default:
+                Console.WriteLine("Isso nao e uma Opção!!");
+                break;
+        }
     }
 }
