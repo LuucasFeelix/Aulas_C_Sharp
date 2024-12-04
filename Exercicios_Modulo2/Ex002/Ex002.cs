7638130a7c72869c5ba2b2cacc5f4e21745305ee
Lucas Felix Da Silva <luucasfeelix1925@hotmail.com>
2024-12-03 22:24:59 -0300

    Realizado Exercicio 2

---
+++ Exercicios_Modulo2/Ex002/Ex002.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Exercicios1{
+    static void Main(){
+        Console.WriteLine("--------------------------------------------------------");
+        Console.WriteLine("Digite um número para saber se e negativo ou positivo!!");
+        Console.WriteLine("--------------------------------------------------------");
+
+        Console.Write("Qual Número voce deseja Saber?: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if(num >= 0){
+            Console.Write("O numero " + num +  " ele é Positivo!!");
+        }else if(num < 0){
+            Console.Write("O numero " + num +  " ele é Negativo!!");
+        }
+    }
+}
